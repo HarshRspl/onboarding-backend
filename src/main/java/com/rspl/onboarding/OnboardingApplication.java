9468e8e4a1cc1dad52099e3121f142e78d042c95
HarshRspl <panditharsh15@gmail.com>
2026-02-23 22:41:53 +0530

    Create OnboardingApplication.java

---
+++ src/main/java/com/rspl/onboarding/OnboardingApplication.java
@@ -0,0 +1,11 @@
+`java
+package com.rspl.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class OnboardingApplication {
+    public static void main(String
