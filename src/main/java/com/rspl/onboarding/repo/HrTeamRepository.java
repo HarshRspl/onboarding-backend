954078d8ae3dab5c556b523a4924d8665597682f
HarshRspl <panditharsh15@gmail.com>
2026-02-25 15:47:56 +0530

    src/main/java/com

--- src/main/java/com/rspl/onboarding/repo/HrTeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rspl.onboarding.repo;
-
-import com.rspl.onboarding.domain.HrTeamMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HrTeamRepository extends JpaRepository<HrTeamMember, Integer> {
-}
